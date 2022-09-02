4f5e3317408f196b2406f9a1d4ecd7ebf2b3398b
Christina Trinh <chrisvt.trinh@gmail.com>
2022-09-02 18:44:27 -0500

    re-add comps

--- src/components/CreateActivity.js
+++ src/components/CreateActivity.js
@@ -1,53 +1,55 @@
-import React from "react";
-import { useState } from "react"
-import { postActivities } from "../api"
+import React from 'react';
+import { useState } from 'react';
+import { postActivities } from '../api';
 function CreateActivity(props) {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [message, setMessage] = useState("")
-  const [setModify] = [props.setModify]
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [message, setMessage] = useState('');
+  const [setModify] = [props.setModify];
   async function submitHandler(event) {
     event.preventDefault();
-    setModify(true)
-    const result = await postActivities(name, description)
+    setModify(true);
+    const result = await postActivities(name, description);
     setMessage(result.message);
     if (!message) {
-      setMessage("Activity Added");
+      setMessage('Activity Added');
     }
-    setName("");
-    setDescription("");
+    setName('');
+    setDescription('');
     setModify(false);
   }
   return (
     <div>
-      <form className="createActivity" onSubmit={submitHandler}>
+      <form className='createActivity' onSubmit={submitHandler}>
         <h2>Create Activity</h2>
         <fieldset>
           <label htmlFor='name'>Name</label>
           <input
             minLength={1}
-            id="name"
-            type="text"
-            placeholder="Name"
+            id='name'
+            type='text'
+            placeholder='Name'
             value={name}
             onChange={(e) => {
               setName(e.target.value);
             }}
-          /></fieldset>
+          />
+        </fieldset>
         <fieldset>
           <label htmlFor='description'>Description</label>
           <input
             minLength={1}
-            id="description"
-            type="text"
-            placeholder="Description"
+            id='description'
+            type='text'
+            placeholder='Description'
             value={description}
             onChange={(e) => {
               setDescription(e.target.value);
             }}
-          /></fieldset>
+          />
+        </fieldset>
         <fieldset>
-          <button type="submit">Submit</button>
+          <button type='submit'>Submit</button>
           <p>{message}</p>
         </fieldset>
       </form>
@@ -55,15 +57,3 @@ function CreateActivity(props) {
   );
 }
 export default CreateActivity;
-
-
-
-
-
-
-
-
-
-
-
-
