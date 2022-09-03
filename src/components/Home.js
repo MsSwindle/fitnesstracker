b09fe4e03fc8190931e711a340c22a4db4d03e40
MsSwindle <msmaryswindle@yahoo.com>
2022-09-02 22:05:40 -0500

    corrected spelling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return (
+	return (
+		<div className="home">
+			<h1>Welcome to Fitness Tracker!</h1>
+			<br />
+			<h3>
+				This is a site for you to find and share your workout routines
+				with others.{' '}
+			</h3>
+			<h3>
+				Please Register/Log In to access your personal routines and
+				activities!
+			</h3>
+			<h3>Join our community and get your routine on!</h3>
+			<br />
 
-    <div className="home">
-    <h1>Welcome to Fitness Trak.r!</h1>
-    <br />
-    <h3>This is a site for you to find and share your workout routines with others. </h3>
-    <h3>Please Register/Log In to access your personal routines and activities!</h3>
-    <h3>Join our community and get your routine on!</h3>
-    <br />
-
-      <p>New User? 
-      <span> <Link to='Register'>Sign up here</Link></span>
-      </p>
-    </div>
-  );
+			<p>
+				New User?
+				<span>
+					{' '}
+					<Link to="Register">Sign up here</Link>
+				</span>
+			</p>
+		</div>
+	);
 };
 
 export default Home;
