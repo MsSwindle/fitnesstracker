a5806d30ef55a08e47e6bfbbb6c6b69e74e9fc72
MsSwindle <msmaryswindle@yahoo.com>
2022-09-02 22:19:40 -0500

    done with stuff now to make it look nice

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,19 +17,15 @@ const Home = () => {
 			<h3>Join our community and get your routine on!</h3>
 			<br />
 
-    <div className="home">
-    <h1>Welcome to Fitness Tracker!</h1>
-    <br />
-    <h3>This is a site for you to find and share your workout routines with others. </h3>
-    <h3>Please Register/Log In to access your personal routines and activities!</h3>
-    <h3>Join our community and get your routine on!</h3>
-    <br />
-
-      <p>New User? 
-      <span> <Link to='/register'>Sign up here</Link></span>
-      </p>
-    </div>
-  );
+			<p>
+				New User?
+				<span>
+					{' '}
+					<Link to="/register">Sign up here</Link>
+				</span>
+			</p>
+		</div>
+	);
 };
 
 export default Home;
